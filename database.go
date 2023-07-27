20ee6b4859dab447865dec4885dc66fda30b54e9
Antonio Bello <ab3llo15@gmail.com>
2023-07-27 16:44:10 +0100

    feat: use generic Type any for model in database Connection

--- database.go
+++ database.go
@@ -14,7 +14,7 @@ type DatabaseConnection struct {
 	DB *gorm.DB
 }
 
-func Connect(cfg config.Config, model interface{}) DatabaseConnection {
+func Connect[T any](cfg config.Config, model T) DatabaseConnection {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/London",
 		cfg.Database.Host,
 		cfg.Database.User,
@@ -39,7 +39,7 @@ func Connect(cfg config.Config, model interface{}) DatabaseConnection {
 	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
 	sqlDB.SetConnMaxLifetime(30 * time.Minute)
 
-	db.AutoMigrate(model)
+	db.AutoMigrate(&model)
 
 	return DatabaseConnection{db}
 }
